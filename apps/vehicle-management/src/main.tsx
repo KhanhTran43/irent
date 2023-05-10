22f7171f30aec9cedcc567a92ac1ae45101ed403
Man Nguyen <nnman.vn@gmail.com>
2023-05-11 01:20:10 +0700

    init: format and lint

--- apps/vehicle-management/src/main.tsx
+++ apps/vehicle-management/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <>Hello I'm app</>
+    <BrowserRouter>
+      <>Hello I'm app</>
+    </BrowserRouter>
   </StrictMode>
 );
