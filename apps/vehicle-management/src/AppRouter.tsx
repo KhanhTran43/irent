580044a97286a11fc27265f04f8f345188d4f704
Tung Nguyen <pod.772000@gmail.com>
2023-07-10 23:06:44 +0800

    feature: remove cypress and implement signup and login form

---
+++ apps/vehicle-management/src/AppRouter.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './containers/Login/Login';
+import SignUp from './containers/SignUp/Signup';
+
+const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
--- apps/vehicle-management/src/main.tsx
+++ apps/vehicle-management/src/main.tsx
@@ -1,14 +1,4 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './AppRouter';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <>Hello I'm app</>
-    </BrowserRouter>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<AppRouter />);
