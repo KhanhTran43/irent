c5c0bf3cba6bbff5c99929b9f10e677909791cf3
Man Nguyen <nnman.vn@gmail.com>
2023-07-24 20:53:23 +0800

    rename vehicle-management to app

--- apps/vehicle-management-e2e/src/e2e/app.cy.ts
+++ apps/vehicle-management-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('vehicle-management', () => {
+describe('app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('vehicle-management', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome vehicle-management');
+    getGreeting().contains('Welcome app');
   });
 });
