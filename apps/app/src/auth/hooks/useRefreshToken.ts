2e6c9771d6832116d25fb9a1f1ebe8fca50cda64
Phu An <phuanbeu530@gmail.com>
2023-08-14 23:48:26 +0700

    feat: persist login 80%

--- apps/app/src/auth/hooks/useRefreshToken.ts
+++ apps/app/src/auth/hooks/useRefreshToken.ts
@@ -1,14 +1,12 @@
-import { api } from '../../axios/axios';
+import { api, privateApi } from '../../axios/axios';
 import { AuthenticateResponse } from '../models';
 import { useAuthStore } from '../store';
 
 export const useRefreshToken = () => {
-  const { authenticate } = useAuthStore(({ authenticate }) => ({
-    authenticate,
-  }));
+  const { user, authenticate } = useAuthStore(({ user, authenticate }) => ({ user, authenticate }));
 
   const refresh = async () => {
-    const response = await api.get<AuthenticateResponse>('auth/refresh', {});
+    const response = await privateApi.post<AuthenticateResponse>(`auth/refresh-token`, {});
 
     const { id, username, jwtToken } = response.data;
 
