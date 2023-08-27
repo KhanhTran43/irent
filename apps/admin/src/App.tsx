478d8d6b543e91d77a0866ce9a53cc0fe3a92a3a
Phu An <phuanbeu530@gmail.com>
2023-08-27 22:41:31 +0800

    feat: admin warehouse

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,50 +1,17 @@
 import './App.css';
 
 import jsonServerProvider from 'ra-data-json-server';
-import { Admin, EditGuesser, Resource, ShowGuesser } from 'react-admin';
+import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser } from 'react-admin';
 
-import { TransactionDetails } from './components/TransactionDetails';
-import { TransactionEdit } from './components/TransactionEdit';
-import { TransactionList } from './components/TransactionList';
-import { WarehouseList } from './components/WarehouseList';
 import { authProvider } from './provider';
 
-// TODO: update api
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-// const dataProvider = jsonServerProvider('https://7477-2001-f40-95c-38e1-1850-aac0-f77b-3419.ngrok.io/evaluationForm');
-
-type Transaction = {
-  id: number;
-  warehouse: {
-    id: number;
-    name: string;
-  },
-  owner: {
-    id: number;
-    name: string;
-  },
-  renter: {
-    id: number;
-    name: string;
-  },
-  price: string;
-  duration: number;
-  createdAt: Date;
-  fee: number
-}
+const dataProvider = jsonServerProvider("http://localhost:7075/api");
 
 const App = () => (
   <Admin authProvider={authProvider} dataProvider={dataProvider}>
-    <Resource edit={TransactionEdit} list={TransactionList} name="transactions" show={TransactionDetails} />
-    <Resource edit={EditGuesser}  list={WarehouseList} name="users" show={ShowGuesser}/>
+    <Resource list={ListGuesser} name="warehouse" show={ShowGuesser}/>
   </Admin>
 );
 
 export default App;
-
-
-// TODO: endpoint
-
-// transactions
-// transactions/:id
 
