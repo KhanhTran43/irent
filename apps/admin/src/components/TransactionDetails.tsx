b810f861346dc46c11a6d5a4abfc468f8fd6cd83
Phu An <phuanbeu530@gmail.com>
2023-08-28 21:04:20 +0700

    chore:admin hide contract when not rented

--- apps/admin/src/components/TransactionDetails.tsx
+++ apps/admin/src/components/TransactionDetails.tsx
@@ -29,38 +29,42 @@ export const TransactionDetails = () => {
         <DateField label="Ngày kết thúc" source="rentedInfo.endDate" />
         <DateField label="Thời gian tạo" showTime={true} source="createdDate" />
         <BooleanField label="Tình trạng" source="rented" valueLabelFalse="Chưa thuê" valueLabelTrue="Đang thuê" />
-        <Button onClick={() => setContractOpen(true)}>
-          <>{'View contract'}</>
-        </Button>
-        <Confirm
-          confirm="Ok"
-          content={
-            <div
-              style={{
-                height: 'calc(100vh - 200px)',
-                width: 1280,
+        {record.rentedInfo && (
+          <>
+            <Button onClick={() => setContractOpen(true)}>
+              <>{'View contract'}</>
+            </Button>
+            <Confirm
+              confirm="Ok"
+              content={
+                <div
+                  style={{
+                    height: 'calc(100vh - 200px)',
+                    width: 1280,
+                  }}
+                >
+                  <embed
+                    src={`data:application/pdf;base64,${record.rentedInfo ? record.rentedInfo.contractBase64 : ''}`}
+                    style={{
+                      height: '100%',
+                      width: 'calc(100% - 2*24px)',
+                    }}
+                    type="application/pdf"
+                  ></embed>
+                </div>
+              }
+              isOpen={isContractOpen}
+              maxWidth={false}
+              scroll="body"
+              style={{ height: '100%' }}
+              title="Contract"
+              onClose={() => {
+                setContractOpen(false);
               }}
-            >
-              <embed
-                src={`data:application/pdf;base64,${record.rentedInfo ? record.rentedInfo.contractBase64 : ''}`}
-                style={{
-                  height: '100%',
-                  width: 'calc(100% - 2*24px)',
-                }}
-                type="application/pdf"
-              ></embed>
-            </div>
-          }
-          isOpen={isContractOpen}
-          maxWidth={false}
-          scroll="body"
-          style={{ height: '100%' }}
-          title="Contract"
-          onClose={() => {
-            setContractOpen(false);
-          }}
-          onConfirm={() => setContractOpen(false)}
-        ></Confirm>
+              onConfirm={() => setContractOpen(false)}
+            ></Confirm>
+          </>
+        )}
       </SimpleShowLayout>
     </Show>
   );
