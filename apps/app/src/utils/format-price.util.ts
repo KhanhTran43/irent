950dba3b99963b96dc04801a61cf43d751a49ae2
huutinhlg2d <phuanbeu530@gmail.com>
2023-09-22 21:22:06 +0700

    refactor: format price ulti

--- apps/app/src/utils/format-price.util.ts
+++ apps/app/src/utils/format-price.util.ts
@@ -1,15 +1,4 @@
 export function formatPrice(price: number): string {
-  // Price receive without 000
-  const priceStr = price.toString();
-  return (
-    priceStr.split('').reduce((prev, curr, idx) => {
-      if ((priceStr.length - idx) % 3 === 0 && idx !== 0) {
-        prev += `,${curr}`;
-      } else {
-        prev += curr;
-      }
-
-      return prev;
-    }, '') + ',000'
-  );
+  price *= 1000;
+  return price.toLocaleString('vi-VN');
 }
